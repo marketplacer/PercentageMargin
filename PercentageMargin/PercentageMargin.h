31d816181dddcd59df6d48541238323e44f5d50b
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-09-28 14:58:47 +1000

    Initial Commit

---
+++ PercentageMargin/PercentageMargin.h
@@ -0,0 +1,19 @@
+//
+//  PercentageMargin.h
+//  PercentageMargin
+//
+//  Created by Evgenii on 28/09/2015.
+//  Copyright © 2015 Marketplacer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PercentageMargin.
+FOUNDATION_EXPORT double PercentageMarginVersionNumber;
+
+//! Project version string for PercentageMargin.
+FOUNDATION_EXPORT const unsigned char PercentageMarginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PercentageMargin/PublicHeader.h>
+
+
